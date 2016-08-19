9732b92daa4c1d34c47c22c48597bee80888db22
Neal Taylor, Jr <neal.taylorjr@gmail.com>
2016-08-19 12:29:49 -0500

    deployment server fix

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -1,7 +1,7 @@
 //database connection
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/events');
-// mongoose.connect(process.env.MONGO_URI);
+// mongoose.connect('mongodb://localhost:27017/events');
+mongoose.connect(process.env.MONGO_URI);
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
